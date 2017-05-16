af3f7129b8ae8ccdccc7ca57642fcf219cf55394
kavunshiva <kavunshiva@gmail.com>
2017-05-16 16:52:51 -0400

    make index-test.js happy

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,23 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
+let counter = 0
+
 function init() {
-  // your code here
+  document.addEventListener('keydown', function(e) {
+    compareCode(e)
+  })
+}
+
+let compareCode = function(e) {
+  let key = parseInt(e.detail || e.which)
+  if(code[counter] === key) {
+    counter++
+    if (counter === (code.length - 1)) {
+      alert("You are now over 9000!")
+      counter = 0
+    }
+  }
+  else {
+    counter = 0
+  }
 }
